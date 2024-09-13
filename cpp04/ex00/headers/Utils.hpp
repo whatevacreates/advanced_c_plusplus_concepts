dfd9832898149c438abed216a707dad1db2b6133
coolasfk <fk.przybyla@gmail.com>
2024-09-13 16:43:14 +0200

    colors

---
+++ cpp04/ex00/headers/Utils.hpp
@@ -0,0 +1,32 @@
+#ifndef _HPP
+# define TOOLS_H_
+
+#include <iostream>
+
+// Extended Pastel Colors
+# define PASTEL_PEACH     "\033[38;5;180m"   // Soft peach
+# define PASTEL_LAVENDER  "\033[38;5;183m"   // Light lavender
+# define PASTEL_MINT      "\033[38;5;121m"   // Mint green
+# define PASTEL_SKY       "\033[38;5;153m"   // Soft sky blue
+# define PASTEL_ROSE      "\033[38;5;210m"   // Rose pink
+# define PASTEL_TURQUOISE "\033[38;5;159m"   // Turquoise
+# define PASTEL_LIME      "\033[38;5;191m"   // Light lime green
+# define PASTEL_CORAL     "\033[38;5;216m"   // Soft coral
+# define PASTEL_IVORY     "\033[38;5;229m"   // Soft ivory
+# define PASTEL_SUN       "\033[38;5;223m"   // Sun yellow
+# define PASTEL_VIOLET    "\033[38;5;183m"   // Soft violet
+# define PASTEL_AQUA      "\033[38;5;159m"   // Aqua blue
+# define PASTEL_SALMON    "\033[38;5;216m"   // Salmon pink
+# define PASTEL_CREAM     "\033[38;5;229m"   // Cream
+
+// Reset Color
+# define C_RESET          "\033[0m"
+
+// Formatting Options (Bold, Italic, etc.)
+# define BOLD             "\033[1m"
+# define ITALIC           "\033[3m"
+# define UNDERLINE        "\033[4m"
+# define STRIKE           "\033[9m"
+# define RESET            "\033[0m"
+
+#endif
