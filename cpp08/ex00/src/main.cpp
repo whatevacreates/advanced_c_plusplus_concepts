c5216698edff7dca6c0a049f2d89784987f8ed84
IMac <imac@iMac-IMac.local>
2024-09-28 19:04:04 +0200

    cpp08

--- cpp08/ex00/src/main.cpp
+++ cpp08/ex00/src/main.cpp
@@ -10,7 +10,7 @@ template<typename T>
 typename T::const_iterator easyfind(const T& container, int value)
 {
 
-    typename T::iterator it;
+    typename T::const_iterator it;
     it = find(container.begin(), container.end(), value);
     if(it == container.end())
     {
@@ -25,14 +25,43 @@ int main()
     int length = sizeof(array[0]/sizeof(array));
     std::vector<int> vectorInt;
     vectorInt.assign(array, array + length);
-    int b = 90;
+    int b = 40;
+    std::cout << B_ROSE << BOLD << ":::::::::::::::::::::::::::::::::::::::: TEST 1: INT VECTOR ::::::::::::::::::::::::::::::::::::::::" << RESET << std::endl;
     try
     {
-        easyfind(vectorInt, b);
+        if(typeid(vectorInt) == typeid(std::vector<int>))
+        {
+            easyfind(vectorInt, b);
+        }
+        else
+        {
+            throw std::runtime_error("The first argument is not the container of intergers");
+            std::cout << ITALIC << B_BABY_BLUE << "Test with container of integers" << RESET << std::endl;
+        }
+            
+        std::cout << "success" << std::endl;
     }
     catch(std::exception &e)
     {
         std::cout << "Exception caught: " << e.what() << std::endl;
     }
-    
+   std::cout << B_ROSE << BOLD << ":::::::::::::::::::::::::::::::::::::::: TEST 2: ARRAY ::::::::::::::::::::::::::::::::::::::::" << RESET << std::endl;
+    try
+    {
+        if(typeid(array) == typeid(std::vector<int>))
+        {
+            easyfind(array, b);
+        }
+        else
+        {
+            throw std::runtime_error("The first argument is not the container of intergers");
+            std::cout << ITALIC << B_BABY_BLUE << "Test with container of integers" << RESET << std::endl;
+        }
+          
+        std::cout << "success" << std::endl;
+    }
+    catch(std::exception &e)
+    {
+        std::cout << "Exception caught: " << e.what() << std::endl;
+    }
 }
