992296fe3001da4bfa036775628e141b9a27e602
coolasfk <fk.przybyla@gmail.com>
2024-09-30 16:33:04 +0200

    progress

--- cpp08/ex01/headers/Span.hpp
+++ cpp08/ex01/headers/Span.hpp
@@ -1,26 +1,26 @@
 #ifndef SPAN_HPP
-#define SPAN_HPP
+# define SPAN_HPP
 
-#include <iostream>
-#include <string>
-#include <set>
+# include <iostream>
+# include <set>
+# include <string>
 
 class Span
 {
-    private:
-    std::set<long long int> _bank;
-    long long int _max;
-    public:
-    Span();
-    Span(long long int max);
-    Span(const Span& other);
-    Span& operator=(const Span& other);
-    ~Span();
-    void addNumber(long long int number);
+  private:
+	std::set<long long int> _bank;
+	long long int _max;
 
-   long long int shortestSpan() const;
-    long long int longestSpan() const;
-    
+  public:
+	Span();
+	Span(long long int max);
+	Span(const Span &other);
+	Span &operator=(const Span &other);
+	~Span();
+	void addNumber(long long int number);
+
+	long long int shortestSpan() const;
+	long long int longestSpan() const;
 };
 
 #endif
--- cpp08/ex01/src/Span.cpp
+++ cpp08/ex01/src/Span.cpp
@@ -1,98 +1,88 @@
 #include "Span.hpp"
 
-Span::Span():_max(0)
-{}
-
-Span::Span(long long int max): _max(max)
-{
-
-}
-
-Span::Span(const Span& other): _bank(other._bank), _max(other._max),
+Span::Span() : _max(0)
 {
 }
 
-Span& Span::operator=(const Span& other)
+Span::Span(long long int max) : _max(max)
 {
-    if(this != &other)
-    {
-        _max = other._max;
-        _bank = other._bank;
-    }
-    return *this;
+}
+
+Span::Span(const Span &other) : _bank(other._bank), _max(other._max),
+{
+}
+
+Span &Span::operator=(const Span &other)
+{
+	if (this != &other)
+	{
+		_max = other._max;
+		_bank = other._bank;
+	}
+	return (*this);
 }
 
 Span::~Span()
-{}
+{
+}
 
 void Span::addNumber(long long int number)
 {
-    try
-    {
-         if((long long int)_bank.size() >= _max)
-    {
-        throw std::runtime_error("The bank is already FULL.");
-    }
-    _bank.insert(number);
-    }
-    catch (std::runtime_error &e)
-    {
-        std::cout << "Exception caught: " << e.what() << std::endl;
-    }
-     catch (std::exception &e)
-    {
-        std::cout << "Exception caught: " << e.what() << std::endl;
-    }
-   
+	try
+	{
+		if ((long long int)_bank.size() >= _max)
+		{
+			throw std::runtime_error("The bank is already FULL.");
+		}
+		_bank.insert(number);
+	}
+	catch (std::runtime_error &e)
+	{
+		std::cout << "Exception caught: " << e.what() << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Exception caught: " << e.what() << std::endl;
+	}
 }
 
-void shortestSpan()
+void	shortestSpan(void)
 {
-    long long int temp;
-    long long int min = MAX_INT;
-    std::set<long long int>::const_iterator it;
-     try
-    {
-         if(_bank.size() <= 1)
-    {
-        throw std::runtime_error("The bank is too small to proceed.");
-    }
-    long long int length = (sizeof(_bank))/sizeof(_bank[0]);
-min =  std::numeric_limits<long long int>::max();
+	long long int	temp;
+	long long int	min;
+	long long int	length;
 
-
-    for(it = std::next(_bank.begin()); it != _bank.end(); ++it)
-    {
-        temp = *it - (--*it);
-if(min > temp)
-{
-    min = temp;
-}
-
-
-    }
-
-
-
-
-std::cout << min << std::endl;
-
-
-
-
-    
-    }
-    catch (std::runtime_error &e)
-    {
-        std::cout << "Exception caught: " << e.what() << std::endl;
-    }
-     catch (std::exception &e)
-    {
-        std::cout << "Exception caught: " << e.what() << std::endl;
-    }
+	min = MAX_INT;
+	std::set<long long int>::const_iterator it;
+	try
+	{
+		if (_bank.size() <= 1)
+		{
+			throw std::runtime_error("The bank is too small to proceed.");
+		}
+		length = (sizeof(_bank)) / sizeof(_bank[0]);
+		min = std::numeric_limits<long long int>::max();
+		for (it = std::next(_bank.begin()); it != _bank.end(); ++it)
+		{
+			temp = *it - (--*it);
+			if (min > temp)
+			{
+				min = temp;
+			}
+		}
+		std::cout << min << std::endl;
+	}
+	catch (std::runtime_error &e)
+	{
+		std::cout << "Exception caught: " << e.what() << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Exception caught: " << e.what() << std::endl;
+	}
 }
 
 long long int Span::longestSpan() const
 {
-    std::cout << *(_bank.end()) - *(_bank.begin()) << std::endl;
+	std::cout << *(_bank.end()) - *(_bank.begin()) << std::endl;
 }
